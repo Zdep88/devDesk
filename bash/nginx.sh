5d7b4003d12e9dcf51b04ff1174906c15d98d6ce
Nicolas Conan <nicolas.conan@gmail.com>
2025-07-12 17:11:14 +0200

    auto delete comment

--- bash/nginx.sh
+++ bash/nginx.sh
@@ -1,11 +1,9 @@
 sudo apt install -y nginx
-printf "\nUncomment line : \nserver_names_hash_bucket_size 64;\n"
-read -n 1 -s -p "Press any key when ready."
-sudo nano /etc/nginx/nginx.conf
-
+sudo sed -i 's/^[[:space:]]*#[[:space:]]*server_names_hash_bucket_size/        server_names_hash_bucket_size/' /etc/nginx/nginx.conf
 read -p "Do you want to create a new site ?(Y/n) " create_site
 if [[ $create_site == "Y" || $create_site == "y" || -z $create_site ]]; then
     read -p "Site url : " site
+    sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$site
     sudo nano /etc/nginx/sites-available/$site
     sudo ln -s /etc/nginx/sites-available/$site /etc/nginx/sites-enabled/
     sudo nginx -t && sudo systemctl restart nginx
