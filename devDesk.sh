56c3490d34ab52e132874afa885eda736ea26065
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-13 12:53:56 +0200

    fix

--- devDesk.sh
+++ devDesk.sh
@@ -4,9 +4,9 @@ read -p "Enter your email : " email
 sudo apt update &&
 sudo apt upgrade -y &&
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash &&
-export NVM_DIR="$HOME/.nvm" &&
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install --lts &&
 nvm use --lts &&
 npm install -g npm@latest &&
