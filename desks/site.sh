9fdccb92c446320554daa0cfb2fdb3c85f9aa606
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-19 22:48:26 +0200

    sh

--- desks/site.sh
+++ desks/site.sh
@@ -9,25 +9,42 @@ fi &&
 gitname=$(basename $giturl .git) &&
 cd ~/ &&
 git clone $giturl &&
-sudo touch /etc/nginx/sites-available/$siteurl &&
-sudo mkdir /var/www/$siteurl &&
-while true; do
-    read -p "Site type ? (static/dynamic/custom) " answer
-    if [ "$answer" == "static" ]; then
-        echo "static" &&
-        sudo cp -r ~/$gitname/* /var/www/$siteurl &&
-        sudo cp ~/devDesk/templates/static_server_bloc.txt /etc/nginx/sites-available/$siteurl &&
-        sed -i "s/URL/$siteurl/g" ~/etc/nginx/sites-available/$siteurl &&
-        break
-    elif [ "$answer" == "dynamic" ]; then
-        echo "dynamic" &&
-        break
-    elif [ "$answer" == "custom" ]; then
-        echo "custom" &&
-        break
-    fi
-done
-sudo certbot --nginx
+echo "Cloned $gitname" &&
+echo "Creating site $siteurl" &&
+echo "gitname : $gitname" &&
+
+
+# if [[ "$answer" == "static" ]]; then
+#     echo "static" &&
+#     sudo cp -r ~/$gitname/* /var/www $siteurl &&
+#     sudo cp ~/devDesk/templates/ static_server_bloc.txt /etc/nginxsites-available/$siteurl &&
+#     sed -i "s/URL/$siteurl/g" ~/etcnginx/sites-available/$siteurl
+#     break
+# fi
+
+# sudo touch /etc/nginx/sites-available/$siteurl &&
+# sudo mkdir /var/www/$siteurl &&
+# while true; do
+#     read -p "Site type ? (static/dynamic/custom) " answer
+#     if [ "$answer" == "static" ]; then
+#         echo "static" &&
+#         sudo cp -r ~/$gitname/* /var/www/$siteurl &&
+#         sudo cp ~/devDesk/templates/static_server_bloc.txt /etc/nginx/sites-available/$siteurl &&
+#         sed -i "s/URL/$siteurl/g" ~/etc/nginx/sites-available/$siteurl &&
+#         break
+#     elif [ "$answer" == "dynamic" ]; then
+#         echo "dynamic" &&
+#         break
+#     elif [ "$answer" == "custom" ]; then
+#         echo "custom" &&
+#         break
+#     fi
+# done
+# sudo certbot --nginx
+
+
+
+
 
 # else
 #     echo "Ports already used :" &&
