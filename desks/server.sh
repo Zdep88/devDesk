89f1083a067a758d9115256ad969f33b102380c7
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-19 20:45:48 +0200

    sh

--- desks/server.sh
+++ desks/server.sh
@@ -3,9 +3,3 @@ sh ~/devDesk/bash/up.sh &&
 sh ~/devDesk/bash/node.sh &&
 sh ~/devDesk/bash/ssh.sh "$email" &&
 cd ~/devDesk/
-echo &&
-echo "bash :" &&
-ls ~/devDesk/bash/ &&
-echo &&
-echo "desks :" &&
-ls ~/devDesk/desks
