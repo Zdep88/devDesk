f11f02d08cb5ad81643b3af4c6b3a5a2e8138a83
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-19 20:33:36 +0200

    sh

--- desks/server.sh
+++ desks/server.sh
@@ -1,5 +1,11 @@
-read -p "Enter your email : " email
+read -p "Enter your email : " email &&
 sh ~/devDesk/bash/up.sh &&
 sh ~/devDesk/bash/node.sh &&
 sh ~/devDesk/bash/ssh.sh "$email" &&
-cd ~/devDesk 
+cd ~/devDesk/
+echo &&
+echo "bash :" &&
+ls ~/devDesk/bash/ &&
+echo &&
+echo "desks :" &&
+ls ~/devDesk/desks
