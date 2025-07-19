47821219ab8b202be7c3672cda38c17de8aa2ba0
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-19 19:37:48 +0200

    sh

--- bash/node.sh
+++ bash/node.sh
@@ -1,5 +1,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash &&
-source ~/.bashrc &&
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install --lts &&
 nvm use --lts &&
 npm install -g npm@latest
