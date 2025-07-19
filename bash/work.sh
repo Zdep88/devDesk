4e4a7bb71ee78c01c96d03653f9d6f4498400f52
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-19 19:10:42 +0200

    sh

--- bash/work.sh
+++ bash/work.sh
@@ -1,4 +1,10 @@
 name=$1 &&
 email=$2 &&
+if [[ -z "$name" ]]; then
+  read -p "Enter your full name : " name
+fi
+if [[ -z "$email" ]]; then
+  read -p "Enter your email : " email
+fi
 echo "name $name" &&
 echo "email $email"
