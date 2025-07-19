4cdfd7eb0c3e948c48af453e906965ea8957ceb4
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-19 18:49:41 +0200

    bash

--- bash/sshkey.sh
+++ bash/sshkey.sh
@@ -1,4 +1,5 @@
-read -p "Enter your email : " email &&ssh-keygen -t ed25519 -C "$email" -f ~/.ssh/id_ed25519 -N "" &&
+read -p "Enter your email : " email &&
+ssh-keygen -t ed25519 -C "$email" -f ~/.ssh/id_ed25519 -N "" &&
 eval "$(ssh-agent -s)" &&
 ssh-add ~/.ssh/id_ed25519 &&
 key=$(cat ~/.ssh/id_ed25519.pub) &&
