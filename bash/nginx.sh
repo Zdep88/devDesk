79fc299ec6ffd524ba2448e043eb847202beb4c0
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-19 19:50:08 +0200

    sh

--- bash/nginx.sh
+++ bash/nginx.sh
@@ -1,4 +1,5 @@
 sudo apt install -y nginx &&
 sudo sed -i 's/^[[:space:]]*#[[:space:]]*server_names_hash_bucket_size/        server_names_hash_bucket_size/' /etc/nginx/nginx.conf &&
+sudo rm /etc/nginx/sites-enabled/default &&
 sudo nginx -t &&
 sudo systemctl restart nginx
