8263502df6793956f2021f9d9d70520e483bb881
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-20 13:23:28 +0200

    sh

--- desks/server.sh
+++ desks/server.sh
@@ -5,3 +5,15 @@ sh ~/devDesk/bash/nginx.sh &&
 sh ~/devDesk/bash/certbot.sh &&
 sh ~/devDesk/bash/ssh.sh "$email" &&
 cd ~/devDesk
+while true; do
+    read -p "Host new site ? (y/n) : " choix
+    case $choix in
+        y|Y)
+            echo "new site"
+            ;;
+        n|N)
+            echo "stop"
+            break
+            ;;
+    esac
+done
--- desks/test.sh
+++ desks/test.sh
@@ -1,17 +1,29 @@
 while true; do
-    read -p "Votre choix (a/b/c) : " choix
+    read -p "Host new site ? (y/n) : " choix
     case $choix in
-        a|A)
-            echo "Exécution de la commande A..."
-            break
+        y|Y)
+            echo "new site"
             ;;
-        b|B)
-            echo "Exécution de la commande B..."
-            break
-            ;;
-        c|C)
-            echo "Exécution de la commande C..."
+        n|N)
+            echo "stop"
             break
             ;;
     esac
 done
+# while true; do
+#     read -p "Votre choix (a/b/c) : " choix
+#     case $choix in
+#         a|A)
+#             echo "Exécution de la commande A..."
+#             break
+#             ;;
+#         b|B)
+#             echo "Exécution de la commande B..."
+#             break
+#             ;;
+#         c|C)
+#             echo "Exécution de la commande C..."
+#             break
+#             ;;
+#     esac
+# done
