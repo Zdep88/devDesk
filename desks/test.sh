6ae55aa024b0065649a19347ab1479f4de33685c
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-20 13:15:35 +0200

    sh

--- desks/test.sh
+++ desks/test.sh
@@ -10,24 +10,16 @@ while true; do
     
     case $choix in
         a|A)
-            echo "Vous avez choisi l'option A"
-            # Remplacez cette ligne par votre commande
             echo "Exécution de la commande A..."
             break
             ;;
         b|B)
-            echo "Vous avez choisi l'option B"
-            # Remplacez cette ligne par votre commande
             echo "Exécution de la commande B..."
             break
             ;;
         c|C)
-            echo "Vous avez choisi l'option C"
-            # Remplacez cette ligne par votre commande
             echo "Exécution de la commande C..."
             break
             ;;
     esac
 done
-
-echo "Script terminé."
